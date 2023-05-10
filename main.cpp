b9ece1bff9fa8c6f483ee5b9ab3886402c50f0f7
八尺 <ilikexff@gmail.com>
2023-05-10 22:52:04 +0800

    日常

--- main.cpp
+++ main.cpp
@@ -7,31 +7,12 @@
 **/
 
 #include "LinkList/_LinkList.h"
-
+#include <iostream>
+using namespace std;
 
 int main()
 {
-    LinkList L = nullptr;
-    CreateLinkList_R(L); // 创建链表
 
-    // 测试GetVal函数
-    int v;
-    if (GetVal(L, 3, v)) {
-        std::cout << "第3个元素为：" << v << std::endl;
-    } else {
-        std::cout << "不存在第3个元素" << std::endl;
-    }
 
-    if (GetVal(L, 5, v)) {
-        std::cout << "第5个元素为：" << v << std::endl;
-    } else {
-        std::cout << "不存在第5个元素" << std::endl;
-    }
-
-    if (GetVal(L, 7, v)) {
-        std::cout << "第7个元素为：" << v << std::endl;
-    } else {
-        std::cout << "不存在第7个元素" << std::endl;
-    }
     return 0;
 }
