8cf53f8115155c6c5d000e8f0a3c0b123855fc9a
八尺 <ilikexff@gmail.com>
2023-05-20 21:22:40 +0800

    :sparkles:头插法创建双链表
    :sparkles:头插法创建双链表

--- LinkList/_DoubleLinkList.cpp
+++ LinkList/_DoubleLinkList.cpp
@@ -23,6 +23,26 @@ bool InitDoubleLinkList(DoubleLinkList &L)
 }
 
 
+void CreateDoubleLinkList_H(DoubleLinkList &L)
+{
+    DoubleLinkList s;
+    int n;
+    L = new Node;
+    L->pre = L->next = nullptr;
+    std::cout <<"Enter the number of elements" << std::endl;
+    std::cin >> n;
+    while (n--)
+    {
+        s = new Node;
+        std::cin >> s->data;
+        if (L->next)
+            L->next->pre = s; //注意，如果L有下一个节点，就直接指向这一步即可
+        s->next = L->next;
+        s->pre = L;
+        L->next = s;
+    }
+}
+
 
 
 
